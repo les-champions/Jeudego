6bf18b1df7395371c76d9da0984038a9c3b0a715
iiazertyuiopii <romain.le-bail@eleves.ec-nantes.fr>
2014-12-15 10:29:19 +0100

    début reader + writer

---
+++ test/jeudego/GroupeDePierresTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeudego;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Random;
+
+/**
+ *
+ * @author IAZERTYUIOPI
+ */
+public class GroupeDePierresTest {
+    
+    public GroupeDePierresTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        
+        
+        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class GroupeDePierres.
+     */
+    @Test
+    public void testajouter() {
+       /* GroupeDePierres instance = new GroupeDePierres;
+        PierrePoint expResult = null;
+        PierrePoint result = instance.ajouter();
+        assertEquals(expResult, result);
+        */
+    }
+    
+}
