2ba4a8d2783f89ca3b1b7387481cdac0ac4a2955
Marc Barnabe <barnabe.marc@gmail.com>
2014-12-15 09:50:13 +0100

    Ajout de la classe GroupeDePierres

---
+++ src/jeudego/GroupeDePierres.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeudego;
+
+/**
+ *
+ * @author marc
+ */
+public class GroupeDePierres {
+    
+    PierrePoint start;
+    Goban ground;
+    
+    
+    public GroupeDePierres(PierrePoint p, Goban g)
+    {
+        start = p;
+        ground = g;
+    }
+    
+    public PierrePoint add()
+    {
+        return null;
+    }
+}
