les-champions/Jeudego

commit 5bdd3d80c25c3e6f2fe0344ff29ae1cb11471060

Fix a bug in partieTerminee

src/jeudego/Goban.java
@@ -23,7 +23,7 @@ public class Goban {
   private PierrePoint pointJoueArray[][];//liste des pierres poses sur le plateau
   private Couleur tour; // qui a son tour
   private int nbrToursPasses = 0; // Le nombre de fois successives qu'un joueur a passé son tour
-  public Logger l;
+  
   
   
   public Goban(int width){
@@ -89,6 +89,7 @@ public class Goban {
   public boolean tourDeJeu(int x, int y) {
     if (estMouvementValide(x,y)) {
       l.log(Level.INFO,tour+" joue en ("+x+","+y+")");
+      nbrToursPasses = 0;
       ajouterPierre(x, y);
       tour = tour.autre();
       return true;

test/jeudego/GobanTest.java
@@ -143,6 +143,7 @@ public class GobanTest {
         System.out.println("partieTerminee");
         Goban instance = new Goban(5);
         assertEquals("La partie n'est pas terminée au début", false, instance.partieTerminee());
+        instance.passerTour();
         instance.tourDeJeu(1,1);
         assertEquals("La partie n'est pas terminée après un tour", false, instance.partieTerminee());
         instance.passerTour();

commit 7cc86d90aa7105db4cd78d3e10ce797d3f5be1e0

Fix compilation error introduced in 5bdd3d80c25

src/jeudego/Goban.java
@@ -23,6 +23,7 @@ public class Goban {
   private PierrePoint pointJoueArray[][];//liste des pierres poses sur le plateau
   private Couleur tour; // qui a son tour
   private int nbrToursPasses = 0; // Le nombre de fois successives qu'un joueur a passé son tour
+  protected Logger l;
   
   
   